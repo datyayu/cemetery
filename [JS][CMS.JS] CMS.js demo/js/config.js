a04713d1c14212fef81c89d01b4af2b586fd56be
AoiTsu <AoiTsu>
2016-01-18 22:40:51 -0800

    Added CMS.js demo

---
+++ [JS][CMS.JS] CMS.js demo/js/config.js
@@ -0,0 +1,62 @@
+$(function() {
+
+	CMS.init({
+
+		// Name of your site or location of logo file ,relative to root directory (img/logo.png)
+		siteName: 'Yayu\'s Test',
+
+		// Tagline for your site
+		siteTagline: 'A CMS.js demo',
+
+		// Email address
+		siteEmail: 'aoitsu3@gmail.com',
+
+		// Name
+		siteAuthor: '@datyayu',
+
+		// Navigation items
+		siteNavItems: [
+			{ name: 'Github', href: 'https://github.com/datyayu', newWindow: false},
+			{ name: 'About'}
+		],
+
+		// Posts folder name
+		postsFolder: 'posts',
+
+		// Homepage posts snippet length
+		postSnippetLength: 120,
+
+		// Pages folder name
+		pagesFolder: 'pages',
+
+		// Site fade speed
+		fadeSpeed: 300,
+
+		// Site footer text
+		footerText: '&copy; ' + new Date().getFullYear() + ' All Rights Reserved.',
+
+		// Mode 'Github' for Github Pages, 'Apache' for Apache server. Defaults
+		// to Github
+		mode: 'Github',
+
+		// If Github mode is set, your Github username and repo name. Defaults
+		// to Github pages branch (gh-pages)
+		githubUserSettings: {
+			username: 'datyayu',
+			repo: 'cemetery'
+		}
+	});
+
+	// Markdown settings
+	marked.setOptions({
+		renderer: new marked.Renderer(),
+		gfm: true,
+		tables: true,
+		breaks: false,
+		pedantic: false,
+		sanitize: true,
+		smartLists: true,
+		smartypants: false
+	});
+
+});
---
+++ [JS][CMS.JS] CMS.js demo/posts/2016-01-18_my-sample-post.md
@@ -0,0 +1,24 @@
+---
+layout: post
+title: Example post
+---
+
+## Hello world
+Ok so if this show on the first try, then cmsjs rocks.
+If don't, then I probably fucked something up.
+
+
+### JS code test.
+```
+    (function() {
+        var hello = 'hello';
+        let number = 1;
+        const array = [1, 2, 3, 4];
+        
+        array
+            .map(number => number + 1)
+            .forEach((number) => {
+                console.log(number);
+            });
+    })();
+```
